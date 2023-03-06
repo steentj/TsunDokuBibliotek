51c02eba884f945016689ce87846b7d58a07b355
steen <steentj@hotmail.com>
2023-03-06 14:46:05 +0100

    Indlagt nowarn for duplicate packages for at få projektet til at bygge på Mac

--- TsunDokuBibliotek/MauiProgram.cs
+++ TsunDokuBibliotek/MauiProgram.cs
@@ -1,21 +1,19 @@
 ﻿using Microsoft.Extensions.Logging;
 
 namespace TsundokuBibliotek;
-
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
         builder
-           .UseMauiApp<App>()
-           .UseMauiCommunityToolkit()
-           .ConfigureFonts(fonts =>
-           {
-               fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-               fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-           });
-
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        });
 #if DEBUG
         //builder.Logging.AddDebug();
 #endif
@@ -25,8 +23,6 @@ public static class MauiProgram
         builder.Services.AddSingleton<SettingsViewModel>();
         builder.Services.AddTransient<BogDetaljerViewModel>();
         builder.Services.AddTransient<BogDetaljer>();
-
         return builder.Build();
     }
 }
-
